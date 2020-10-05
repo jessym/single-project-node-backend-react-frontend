bd13193b83450973c01b3c3e2e715840869093b1
Jessy <jessym@jessym.com>
2020-10-05 16:34:13 +0200

    Make the server port configurable

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'static')));
